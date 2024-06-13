2a219e15db325078a12234a1aa8424b3c201b70c
Jon Henrik Bjørnstad <jonhenrik@qbee.io>
2024-06-13 14:14:01 +0200

    Simplify entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,18 @@
 # Container image that runs your code
-FROM alpine:3.10
+FROM alpine:3.19.1
+ENV QBEE_CLI_VERSION="v1.2024.24"
+ENV QBEE_CLI_CHECKSUM="555dbc6ca1f4c7ed3dc06c86e0c6bc1fde2931e74b671a269a278add8419fc47"
 
-RUN apk update \
- && apk add --update --no-cache \
-    jq \
-    curl \
-    bash
+ADD https://github.com/qbee-io/qbee-cli/releases/download/${QBEE_CLI_VERSION}/qbee-cli-${QBEE_CLI_VERSION}.linux-amd64 \
+    /usr/local/bin/
 
-# Copies your code file from your action repository to the filesystem path `/` of the container
+RUN chmod +x /usr/local/bin/qbee-cli-${QBEE_CLI_VERSION}.linux-amd64 && \
+    ln -s /usr/local/bin/qbee-cli-${QBEE_CLI_VERSION}.linux-amd64 /usr/local/bin/qbee-cli
+
+RUN echo "${QBEE_CLI_CHECKSUM}  /usr/local/bin/qbee-cli" | sha256sum -c -
+
+    # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
-ENTRYPOINT ["bash", "/entrypoint.sh"]
+ENTRYPOINT ["sh", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,79 +1,18 @@
-#!/bin/bash -l
+#!/bin/sh
 
 #inputs
-token=$1
 filename=$2
 qbee_directory=$3
 local_directory=$4
 run=$5
 api_host=$6
 
-echo -e "init token: $token\nfilename: $filename\nqbee_directory: $qbee_directory\nlocal_directory: $local_directory\nrun: $run\napi_host: $api_host"
-
-if [ $run != 1 ]; then
-    echo "run is set to false: all files up to date - not performing upload"
-    exit 0
-else
-    echo "running file-upload-action for $filename"
+if [[ -n "$api_host" ]]; then
+  echo "WARNING: api_host is deprecated, this is handled in the authentication action"
 fi
 
-successful_status_code='200'
-
-#get absolute path and add trailing slash
-local_directory="$(cd $local_directory && pwd)/"
-
-#make sure qbee_directory has both leading and trailing slash
-[[ "${qbee_directory}" != /* ]] && qbee_directory="/${qbee_directory}"
-[[ "${qbee_directory}" != */ ]] && qbee_directory="${qbee_directory}/"
-
-apiOutput=$(curl --request "DELETE" -sL -d "{\"path\":\"$qbee_directory$filename\"}" \
-            -H "Content-type: application/json" \
-            --url "https://$api_host/api/v2/file" \
-            --header 'Authorization: Bearer '"$token" \
-            -w "\n{\"http_code\":%{http_code}}\n")
-
-
-echo 'DELETE request'
-http_code=$(echo $apiOutput | jq -cs | jq -r '.[1].http_code')
-
-echo "API output is:"
-echo $apiOutput
-
-if [ "$http_code" == "null" ]; then
-    http_code=$(echo $apiOutput | jq -cs | jq -r '.[0].http_code')
-else
-    :
-    #echo "http_code was not null"
+if [[ -n "$run" ]]; then
+  echo "WARNING: run is deprecated."
 fi
 
-if [[ "$http_code" != "$successful_status_code" && "$http_code" != "404" && "$http_code" != "204" ]]; then
-    echo "http_code was - $http_code"
-    echo "something went wrong ... aborting"
-    exit 1
-elif [ "$http_code" == "404" ]; then
-    echo "file not found ... not a problem ... continuing"
-else
-    echo "http_code was - $http_code"
-fi
-
-apiPostOutput=$(curl --request POST -sL -H "Content-Type: multipart/form-data" \
-               -F "path=$qbee_directory" -F "file=@$local_directory$filename" \
-               --url "https://$api_host/api/v2/file" \
-               --header 'Authorization: Bearer '"$token"\
-               -w "\n{\"http_code\":%{http_code}}\n")
-
-echo 'POST request'
-post_http_code=$(echo $apiPostOutput | jq -cs | jq -r '.[1].http_code')
-
-echo "API Post output is:"
-echo $apiPostOutput
-
-if [ "$post_http_code" != "$successful_status_code" ]
-
-then
-    echo "http_code was - $post_http_code"
-    echo "something went wrong ... aborting"
-    exit 1
-else
-    echo "http_code was - $post_http_code"
-fi
+qbee-cli files upload --source "$local_directory/$filename" --destination "$qbee_directory" --overwrite
